fe23069c276b937bf47e37a686f584bd897362f2
Ajit1803 <64146957+Ajit1803@users.noreply.github.com>
2020-04-22 16:06:04 +0200

    change in import directory

--- app.module.ts
+++ app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header.component';
 @NgModule({
   declarations: [
     AppComponent,
